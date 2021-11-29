103ecae0f3257307bed0c140e2bdaa24651b773c
Web ve Mobil Öğle <Web ve Mobil le@DESKTOP-8M7D7GE>
2021-11-29 16:17:03 +0300

    Migrations

---
+++ TicketApp/Migrations/20211129130842_songul.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicketApp.Migrations
+{
+    public partial class songul : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Employees");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
